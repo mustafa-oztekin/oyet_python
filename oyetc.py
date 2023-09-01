cdc1feed46e2352e64c2776c353dbaef8b45a9da
mustafa-oztekin <mustafaoztekin564@gmail.com>
2023-09-01 11:06:12 +0300

    arduino'dan gelen veriler için try-except bloğu...

--- oyetc.py
+++ oyetc.py
@@ -10,7 +10,7 @@ last_post_time = 0  # Zaman damgasını saklamak için bir değişken
 
 modul = ""
 
-arduino = serial.Serial(port='COM26', baudrate=9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=0)
+arduino = serial.Serial(port='COM10', baudrate=9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=0)
 
 #headersize = 10
 
@@ -23,37 +23,42 @@ url = "http://127.0.0.1:8000"
 while True:
     # burası c modülünün arduinosunda gelen doğrudan yangın verisi için A1'de yangın var gibi.
     if arduino.in_waiting:
-        arduino_veri = arduino.readline()
-        cleaned_data = arduino_veri.replace(b'\r\n', b'')
-        cleaned_data = cleaned_data.decode('utf')
-        words = cleaned_data.split('<>')
-        print(words)
+        try:
+            arduino_veri = arduino.readline()
+            cleaned_data = arduino_veri.replace(b'\r\n', b'')
+            #print(cleaned_data)
+            cleaned_data = cleaned_data.decode('utf-8')
+            words = cleaned_data.split('<>')
+            print(words)
 
 
-        if (len(words) == 2 and len(words[0]) == 3):
-            s.send(words[1].encode())
-            
-        elif (len(words) == 2 and len(words[0]) == 2):
-            # Zaman değeri güncelleme
-            current_time = time.time()
+            if (len(words) == 2 and words[0][2] == "K"):
+                s.send(words[1].encode())
 
-            # Belirli bir süre (örn. 2 dakika) içerisinde aynı veri gelmemişse post işlemini yapar
-            if (current_time - last_post_time > 180 or words[0] != modul):
-            #    if post_counter < max_post_count:  # Sayaç değeri belirtilen maksimum sayıya ulaşmadan post işlemi yapar
-            #    if (words[0] != ""): # boş gelen veri durumunda post yapmasın
-                response = requests.post(url + "/events/" + words[0])
-                print("İstek durumu:", response.status_code)
-                #    post_counter += 1  # Sayaç değerini artır
-                modul = words[0]
+            elif (len(words) == 2 and words[0][2] == "."):
+                # Zaman değeri güncelleme
+                current_time = time.time()
 
-                # Zaman damgasını günceller
-                last_post_time = current_time
-            #else: sadece süreyle kontrol etmek için burayı kapatmayı deniyorum
-                # Zaman süresi içerisinde aynı veri gelmişse, post işlemi yapılmaz ve sayaç sıfırlanır
-            #   post_counter = 0
+                # Belirli bir süre (örn. 2 dakika) içerisinde aynı veri gelmemişse post işlemini yapar
+                if (current_time - last_post_time > 180 or words[0] != modul):
+                #    if post_counter < max_post_count:  # Sayaç değeri belirtilen maksimum sayıya ulaşmadan post işlemi yapar
+                #    if (words[0] != ""): # boş gelen veri durumunda post yapmasın
+                    response = requests.post(url + "/events/" + words[0][:2])
+                    print("İstek durumu:", response.status_code)
+                    #    post_counter += 1  # Sayaç değerini artır
+                    modul = words[0]
+
+                    # Zaman damgasını günceller
+                    last_post_time = current_time
+                #else: sadece süreyle kontrol etmek için burayı kapatmayı deniyorum
+                    # Zaman süresi içerisinde aynı veri gelmişse, post işlemi yapılmaz ve sayaç sıfırlanır
+                #   post_counter = 0
+        except Exception as e:
+            print(e)
+            pass
             
     try:
-        msg = s.recv(3)
+        msg = s.recv(5)
         if msg:
             msg = msg.decode("utf-8")
             print("Received:", msg)
