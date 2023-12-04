eb0b9d310c0b385f1038d47eda456af30198cdb0
mustafa-oztekin <mustafaoztekin564@gmail.com>
2023-12-04 21:53:02 +0300

    son değişiklikler

--- main_oyet.py
+++ main_oyet.py
@@ -1,3 +1,7 @@
+# ilk önce bu python kodu çalıştırılmalı
+# satır 24'te kod client'tan bağlantı bekler
+# client'tan bağlantı onaylanırsa kod alt satırlara devam eder.
+
 from fastapi import FastAPI, status, HTTPException
 from pydantic import BaseModel
 from typing import Optional, List
@@ -9,7 +13,7 @@ from sse_starlette.sse import EventSourceResponse
 import socket
 import asyncio
 
-
+# socket bağlantı için ip ve port
 ip_address = socket.gethostname()
 port = 5000
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -54,6 +58,17 @@ class Event(BaseModel):
 
 db = SessionLocal()
 
+liste = ["hello world"]
+@app.get("/home")
+async def get_liste():
+    return liste
+
+@app.post("/server")
+async def add_liste(veriler: dict):
+    liste.append(veriler)
+    return {"message": "Liste güncellendi", "list": liste}
+
+
 # tum modulleri dondurur
 @app.get("/items", response_model=List[Item], status_code=200)
 async def get_all_items():
@@ -75,14 +90,19 @@ async def get_an_item(item_name:str):
 
 # baslangic sorgulari
 sorgu_event = db.query(models_oyet.Event).all()
-for event_sonuc in sorgu_event:
-    pass
-sonuc_id = event_sonuc.id
+sonuc_id = sorgu_event[-1].id
+#for event_sonuc in sorgu_event:
+#    pass
+#sonuc_id = event_sonuc.id
 
-sorgu_event1 = db.query(models_oyet.Event).all()
-for event_sonuc1 in sorgu_event1:
-    pass
-sonuc_id1 = event_sonuc1.id
+#sonuc_id1 = sorgu_event[-1].id
+#print(sonuc_id1)
+
+#sorgu_event1 = db.query(models_oyet.Event).all()
+#for event_sonuc1 in sorgu_event1:
+#    pass
+#sonuc_id1 = event_sonuc1.id
+#print(sonuc_id1)
 
 
 # frontend ile olan baglanti
@@ -90,16 +110,17 @@ sonuc_id1 = event_sonuc1.id
 async def sse_endpoint():
     async def event_generator():
         global sonuc_id
-        global sonuc_id1
+        #global sonuc_id1
         global event_sonuc1
         # await kullanabilirsin
-        sorgu_event1 = db.query(models_oyet.Event).all()
-        for event_sonuc1 in sorgu_event1:
-            pass
-        sonuc_id1 = event_sonuc1.id
+        sorgu_event = db.query(models_oyet.Event).all()
+        sonuc_id1 = sorgu_event[-1].id
+        #for event_sonuc1 in sorgu_event1:
+        #    pass
+        #sonuc_id1 = event_sonuc1.id
         if (sonuc_id1 > sonuc_id):
             print(sonuc_id1)
-            yield event_sonuc1.modul_name
+            yield sorgu_event[-1].modul_name # frontent'e veri basar
             olay_zaman1 = datetime.now()
             if (olay_zaman1.minute >= olay_zaman.minute + 1):
                 sonuc_id = sonuc_id1
@@ -115,20 +136,32 @@ async def sse_endpoint():
 
 
 
-
+# çemberlere tıklandığında gerçekleşen endpoint
 @app.post("/tcp")
 def listen_to_the_modul(data: dict):
     try:
-        veri = data.get('modul')
-        clientsocket.send(veri.encode())
-
-        print(veri)
-        clientsocket.settimeout(8)  # 10 saniye süreyle socket'ten veri bekliyoruz
-        msg = clientsocket.recv(12)
-        result = msg.decode()
-
+        sayac = 0
+        #veri = data.get('id')
+        result = ""
+        for key, value in data.items():
+            sayac += 1
+            result += str(value) + "|"
+            if sayac == 7:
+                result += "\r"
+                break
         print(result)
-        return {"success": True, "data": result}
+        clientsocket.send(result.encode())
+
+
+        #print(veri)
+        clientsocket.settimeout(5)  # 10 saniye süreyle socket'ten veri bekliyoruz
+        msg = clientsocket.recv(58)
+        #msg = msg.replace(b'\r', b'')
+        msg = msg.decode('utf-8')
+        print(msg)
+        #liste_msg = msg.split("|")
+        #print(liste_msg)
+        return {"success": True, "data": msg}
 
     except socket.timeout:
         print("Timeout Hatası")
@@ -136,10 +169,31 @@ def listen_to_the_modul(data: dict):
     except Exception as e:
         print("Beklenmeyen bir hata oluştu:", str(e))
         return {"success": False, "data": "Beklenmeyen bir hata oluştu"}
-    
 
 
+# yeni bir olay kaydeder
+@app.post("/events/{modul_id}", response_model=Event, status_code=status.HTTP_201_CREATED)
+async def create_an_event(modul_id:str):
+    global sonuc_id
+    global olay_zaman
+    sorgu = db.query(models_oyet.Item).filter(models_oyet.Item.name == modul_id).first()
+    sorgu_event = db.query(models_oyet.Event).all()
+    sonuc_id = sorgu_event[-1].id
+    #for event_sonuc in sorgu_event:
+    #    pass
+    #sonuc_id = event_sonuc.id
 
+    new_event = models_oyet.Event(
+        modul_name = sorgu.name,
+        fire_zone = sorgu.zone, 
+        fire_date = datetime.now(),
+        enlem = sorgu.enlem,
+        boylam = sorgu.boylam
+    )
+    olay_zaman = datetime.now()
+    db.add(new_event)
+    db.commit()
+    return new_event
 
 
 
@@ -160,31 +214,6 @@ async def create_an_item(item:Item):
     db.add(new_item)
     db.commit()
     return new_item
-
-
-
-# yeni bir olay kaydeder
-@app.post("/events/{modul_id}", response_model=Event, status_code=status.HTTP_201_CREATED)
-async def create_an_event(modul_id:str):
-    global sonuc_id
-    global olay_zaman
-    sorgu = db.query(models_oyet.Item).filter(models_oyet.Item.name == modul_id).first()
-    sorgu_event = db.query(models_oyet.Event).all()
-    for event_sonuc in sorgu_event:
-        pass
-    sonuc_id = event_sonuc.id
-
-    new_event = models_oyet.Event(
-        modul_name = sorgu.name,
-        fire_zone = sorgu.zone, 
-        fire_date = datetime.now(),
-        enlem = sorgu.enlem,
-        boylam = sorgu.boylam
-    )
-    olay_zaman = datetime.now()
-    db.add(new_event)
-    db.commit()
-    return new_event
 
 
 @app.put("/item/{item_id}", response_model=Item, status_code=status.HTTP_200_OK)
@@ -209,6 +238,7 @@ async def delete_an_item(item_id:int):
     db.delete(item_to_delete)
     db.commit()
     return item_to_delete
+
 
 if __name__ == "__main__":
     import uvicorn
